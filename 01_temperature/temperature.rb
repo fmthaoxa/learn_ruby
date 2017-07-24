062d2201360da4dba30cecd91713fee36e001cb1
Fwjchim <fwjchim.thaoxaochay@gmail.com>
2017-07-23 23:18:47 -0700

    Finished temperature and thermometer

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,8 @@
 #write your code here
+def ftoc(temp)
+  return (temp-32) * (5.0/9.0)
+end
+
+def ctof(c)
+  return (c*9.0/5.0) + 32
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,32 @@
 #write your code here
+def add(x,y)
+  return x+y
+end
+def subtract(a,b)
+  return a-b
+end
+
+def sum(nums)
+  count = 0
+  if nums.length == 0
+    return 0
+  else
+    return nums.reduce(:+)
+  end
+end
+
+def multiply(nums)
+  return nums.inject(:*)
+end
+
+def power(x,y)
+  return x**y
+end
+
+def factorial(n)
+  if n == 0
+    return 1
+  else
+    return (1..n).inject(:*)
+  end
+end
